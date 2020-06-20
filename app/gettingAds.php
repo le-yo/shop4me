05b874c4e12b8a3bab76922119c80da0a8c16b2e
LaxcoAds <65868300+Laxco@users.noreply.github.com>
2020-06-20 17:47:06 +0300

    Update gettingAds.php

--- app/gettingAds.php
+++ app/gettingAds.php
@@ -14,7 +14,7 @@
 	
 	
 	//creating a query
-	$stmt = $conn->prepare("SELECT * FROM ads;");
+	$stmt = $conn->prepare("SELECT * FROM ads ORDER BY id DESC;");
 	
 	//executing the query 
 	$stmt->execute();
