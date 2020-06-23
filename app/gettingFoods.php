10ad6f8e9da865ba6a4134ee99ccaeaf2ad78ebb
LaxcoAds <65868300+Laxco@users.noreply.github.com>
2020-06-23 05:32:53 -0700

    Add files via upload

---
+++ app/gettingFoods.php
@@ -0,0 +1,50 @@
+<?php 
+	
+	require_once('dbConnect.php');
+	
+	//Checking if any error occured while connecting
+	if (mysqli_connect_errno()) {
+		echo "Failed to connect to MySQL: " . mysqli_connect_error();
+		die();
+	}
+	
+	
+	
+	//creating a query
+	$stmt = $conn->prepare("SELECT * FROM posts WHERE qty > '0' AND qty != 'NULL' AND category RLIKE 'FOOD' ORDER BY dateCreated DESC LIMIT 4;");
+	
+	//executing the query 
+	$stmt->execute();
+	
+	//binding results to the query 
+	$stmt->bind_result($title, $price, $descr, $dateCreated, $imageUrl,$documentId,$category,$size,$brand,$prev_price,$user_id,$shop_id,$qty);
+	
+	$products['posts'] = array(); 
+	
+	//traversing through all the result 
+	while($stmt->fetch()){
+
+		$temp = array();
+		
+		$temp['title'] = $title; 
+		$temp['price'] = $price; 
+		$temp['descr'] = $descr; 
+		$temp['dateCreated'] = $dateCreated; 
+		$temp['imageUrl'] = $imageUrl; 
+		$temp['documentId'] = $documentId; 
+		$temp['category'] = $category;   
+		$temp['size'] = $size;  
+		$temp['brand'] = $brand;
+		$temp['user_id'] = $user_id;
+		$temp['shop_id'] = $shop_id;
+		$temp['prev_price'] = $prev_price;
+		$temp['qty'] = $qty;
+
+		array_push($products['posts'], $temp);
+	}
+	
+	//displaying the result in json format 
+	echo json_encode($products);
+	
+
+?>
