756f4060623cb056a3287fdd1023f65f166af776
LaxcoAds <65868300+Laxco@users.noreply.github.com>
2020-07-02 13:58:55 +0300

    Update chekingIfExists.php

--- app/chekingIfExists.php
+++ app/chekingIfExists.php
@@ -3,30 +3,19 @@ if($_SERVER['REQUEST_METHOD']=='POST'){
 	
 	require_once('dbConnect.php');
 	
-	$id_to_chek = $_POST['id_to_chek'];
-	$table = $_POST['table'];
 	$id = $_POST['id'];
 
 
-	$sql = "SELECT * FROM '$table' WHERE '$id_to_chek' = '$id'";
+	$sql = "SELECT * FROM drivers WHERE driver_id = '$id'";
 	
 	$check = mysqli_fetch_array(mysqli_query($conn,$sql));
-	//$result = $conn->query($sql);
-
-	//$response = array();
 
 	if(isset($check)) {
-
-        /*$response["status"] = "0";
-        $response["message"] = "Exixts.";*/
 		echo '0';
 
 	} else {
-        /*$response["status"] = "1";
-        $response["message"] = "Not there..!";*/
 		echo '1';
 	}
-       // echo json_encode($response);
 
 }
 
