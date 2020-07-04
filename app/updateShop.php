8fc96d3991215bbc5da566990f87fe0275355b70
LaxcoAds <65868300+Laxco@users.noreply.github.com>
2020-07-04 14:13:17 +0300

    Update updateShop.php

--- app/updateShop.php
+++ app/updateShop.php
@@ -6,7 +6,7 @@
     
     if(isset($_REQUEST['user_id']) && isset($_REQUEST['email'])
     && isset($_REQUEST['name']) && isset($_REQUEST['till'])
-    && isset($_REQUEST['phone'])){
+    && isset($_REQUEST['phone']) && isset($_REQUEST['account_no'])){
         
         	//Getting post data 
 		$strUserId = $_REQUEST['user_id'];
@@ -14,8 +14,9 @@
 		$strEmail = $_REQUEST['email'];
 		$strPhone = $_REQUEST['phone'];
 		$strTill = $_REQUEST['till'];
+		$account_no = $_REQUEST['account_no'];
 
-        $adding = "UPDATE shops SET name = '$strName', email = '$strEmail', till = '$strTill', phone = '$strPhone' WHERE user_id = '$strUserId'";
+        $adding = "UPDATE shops SET name = '$strName', email = '$strEmail', till = '$strTill', phone = '$strPhone' ,account_no = '$account_no' WHERE user_id = '$strUserId'";
        
        $result = mysqli_query($conn,$adding);
         
