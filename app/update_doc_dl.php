d901cb7e48fe3e9d18182842fb96b3c2d5547adb
LaxcoAds <65868300+Laxco@users.noreply.github.com>
2020-07-05 12:25:58 +0300

    Update all

---
+++ app/update_doc_dl.php
@@ -0,0 +1,33 @@
+<?php
+  
+    require_once('dbConnect.php');
+    
+    $response = array(); 
+
+ if(isset($_REQUEST['driver_id']) && isset($_REQUEST['dl'])){
+        
+        	//Getting post data 
+		$driver_id = $_REQUEST['driver_id'];
+		$dl = $_REQUEST['dl'];
+
+
+
+        $updating = "UPDATE driver_doc SET dl = '$dl' WHERE driver_id = '$driver_id'";
+       
+       $result_update = mysqli_query($conn,$updating);
+
+//updating        
+    if ($result_update) {
+        // successfully inserted into database
+        $response["status"] = "0";
+        $response["message"] = "Saved.";
+    } else {
+        // failed to insert row
+        $response["status"] = "1";
+        $response["message"] = "Error: ".mysqli_error($con);
+    }
+    // echoing JSON response
+    echo json_encode($response);
+        
+    }
+?>
