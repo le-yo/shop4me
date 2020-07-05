bd885aeda64404ae58107de8228fa02d16dfdc4f
LaxcoAds <65868300+Laxco@users.noreply.github.com>
2020-07-05 21:35:36 +0300

    Add files via upload

---
+++ app/gettingDriversForRide.php
@@ -0,0 +1,50 @@
+<?php 
+	if($_SERVER['REQUEST_METHOD']=='POST'){
+	
+	require_once('dbConnect.php');
+	
+	//Checking if any error occured while connecting
+	if (mysqli_connect_errno()) {
+		echo "Failed to connect to MySQL: " . mysqli_connect_error();
+		die();
+	}
+
+		$vehicle_type = $_POST['vehicle_type'];
+
+	
+	
+	
+	//creating a query
+	$stmt = $conn->prepare("SELECT * FROM drivers WHERE approved = 'true' AND vehicle_type = '$vehicle_type' ORDER BY full_name DESC;");
+	
+	//executing the query 
+	$stmt->execute();
+	
+	//binding results to the query 
+	$stmt->bind_result($driver_id,$full_name,$id,$phone,$vehicle_type,$plate,$date_joined,$location,$approved);
+	
+	$products['drivers'] = array(); 
+	
+	//traversing through all the result 
+	while($stmt->fetch()){
+
+		$temp = array();
+		
+		$temp['driver_id'] = $driver_id;
+		$temp['full_name'] = $full_name;
+		$temp['id'] = $id;
+		$temp['phone'] = $phone;
+		$temp['vehicle_type'] = $vehicle_type;
+		$temp['plate'] = $plate;
+		$temp['date_joined'] = $date_joined;
+		$temp['location'] = $location;
+		$temp['approved'] = $approved;
+
+		array_push($products['drivers'], $temp);
+	
+	}
+    echo json_encode($products);
+}
+	
+
+	?>
