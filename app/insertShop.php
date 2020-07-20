1a42657f190ad964b3939538566d85811e7aa73b
LaxcoAds <65868300+Laxco@users.noreply.github.com>
2020-07-20 10:46:49 +0300

    adding shop location

--- app/insertShop.php
+++ app/insertShop.php
@@ -10,7 +10,7 @@
     && isset($_REQUEST['email']) && isset($_REQUEST['phone'])
     && isset($_REQUEST['user_id']) && isset($_REQUEST['city'])
     && isset($_REQUEST['till']) && isset($_REQUEST['status'])
-    && isset($_REQUEST['account_no'])){
+    && isset($_REQUEST['account_no']) && isset($_REQUEST['latlng'])){
         
         	//Getting post data 
 		$id = $_REQUEST['id'];
@@ -24,11 +24,12 @@
 		$city = $_REQUEST['city'];
 		$user_id = $_REQUEST['user_id'];
 		$till = $_REQUEST['till'];
+		$latlng = $_REQUEST['latlng'];
 		$status = $_REQUEST['status'];
 		$open = $_REQUEST['open'];
 		$account_no = $_REQUEST['account_no'];
 
-        $adding = "INSERT INTO shops (id,name,image,dateCreated,location,category,email,phone,city,user_id,till,status,open,account_no) VALUES ('$id','$name','$image','$dateCreated','$location','$category','$email','$phone','$city','$user_id','$till','$status','$open','$account_no')";
+        $adding = "INSERT INTO shops (id,name,image,dateCreated,location,category,email,phone,city,user_id,till,status,open,account_no,latlng) VALUES ('$id','$name','$image','$dateCreated','$location','$category','$email','$phone','$city','$user_id','$till','$status','$open','$account_no','$latlng')";
        
        $result = mysqli_query($conn,$adding);
         
