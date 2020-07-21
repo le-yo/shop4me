27c99100768404a65b820fa0363e26a7a7f29f41
LaxcoAds <65868300+Laxco@users.noreply.github.com>
2020-07-21 12:18:17 +0300

    Update update_default_locations_single.php

--- app/update_default_locations_single.php
+++ app/update_default_locations_single.php
@@ -1,19 +1,21 @@
 <?php
-   if($_SERVER['REQUEST_METHOD']=='POST'){
+
     require_once('dbConnect.php');
     
-    $response = array(); 
+    
+ if(isset($_REQUEST['user_id']) && isset($_REQUEST['something_to_update']) && isset($_REQUEST['val'])){
         
         	//Getting post data 
-		$user_id = $_REQUEST['user_id'];
-		$something_to_update = $_REQUEST['something_to_update'];
-		$val = $_REQUEST['val'];
+       $response = array(); 
+       $user_id = $_REQUEST['user_id'];
+       $something_to_update = $_REQUEST['something_to_update'];
+       $val = $_REQUEST['val'];
 
 
 
         $updating = "UPDATE default_locations SET '$something_to_update' = '$val' WHERE user_id = '$user_id'";
        
-       $result_update = mysqli_query($conn,$updating);
+        $result_update = mysqli_query($conn,$updating);
 
 //updating        
     if ($result_update) {
