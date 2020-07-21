63908915e23392629998ca29836bbb7b8ea33906
LaxcoAds <65868300+Laxco@users.noreply.github.com>
2020-07-21 11:52:39 +0300

    Update update_default_locations_single.php

--- app/update_default_locations_single.php
+++ app/update_default_locations_single.php
@@ -23,7 +23,7 @@
     } else {
         // failed to insert row
         $response["status"] = "1";
-        $response["message"] = "Error: ".mysqli_error($con);
+        $response["message"] = "Error: ".mysqli_error($updating);
     }
     // echoing JSON response
     echo json_encode($response);
