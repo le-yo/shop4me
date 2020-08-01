5dae85f3e005aaea3e25982b1be6a2325f59accf
LaxcoAds <65868300+Laxco@users.noreply.github.com>
2020-08-01 03:22:07 -0400

    Update callback_url.php

--- app/callback_url.php
+++ app/callback_url.php
@@ -1,6 +1,7 @@
 <?php
-include 'dbConnect.php' ;
 
+
+ include 'dbConnect.php' ;
 
   header("Content-Type: application/json");
   $stkCallbackResponse = file_get_contents('php://input');
@@ -10,49 +11,41 @@ include 'dbConnect.php' ;
   fwrite($log, $stkCallbackResponse);
   fclose($log);
 
-  $AccountReference = $_GET['strOrderId'];
+  $AccountReference = $_GET['strOrderId']; //getting the order id from stkinitiate file
+
+  $jsonMpesaResponse = json_decode($stkCallbackResponse, true);
+
+  $json_data = json_encode($jsonMpesaResponse);
+
+  //initialiing response
+  $response = array(); 
+
+
+  //getting the decoded values
+  $ResultCode=$jsonMpesaResponse["Body"]["stkCallback"]["ResultCode"];
+  $MerchantRequestID=$json_data["Body"]["stkCallback"]["MerchantRequestID"];//confirmed
+  $CheckoutRequestID=$jsonMpesaResponse["Body"]["stkCallback"]["CheckoutRequestID"];
+  $ResultDesc=$jsonMpesaResponse["Body"]["stkCallback"]["ResultDesc"];
+  $Amount=$jsonMpesaResponse["Body"]["stkCallback"]["CallbackMetadata"]["Item"][0]["Value"];
+  $ReceiptNumber=$jsonMpesaResponse["Body"]["stkCallback"]["CallbackMetadata"]["Item"]["stk_initiate"][1]["Value"];
+  $TransactionDate=$jsonMpesaResponse["Body"]["stkCallback"]["CallbackMetadata"]["Item"][3]["Value"];
+  $PhoneNumber=$jsonMpesaResponse["Body"]["stkCallback"]["CallbackMetadata"]["Item"][4]["Value"];
+   
+   //checking if transaction was successful
+   if($ResultCode == 0){  	
+	$query = "INSERT INTO mpesa_account (strOrderId,strPhone,strAmount) VALUES ('$AccountReference','$PhoneNumber','$Amount')";
+	$result = mysqli_query($conn,$adding);//inserting transaction to db after a=successul transaction
+	   
+	$response["status"] = "0";
+        $response["message"] = "Successfuly sents.";
+	   
+   }else {
+	   
+	//the transaction was not successfull...
+	$response["status"] = "0";
+        $response["message"] = "Successfuly sents.";
+   }
  
-     $jsonMpesaResponse = json_decode($stkCallbackResponse, true);
-     $KILI = json_encode($jsonMpesaResponse);
-     $ResultCode=$jsonMpesaResponse["Body"]["stkCallback"]["ResultCode"];
-    // $ResultCode=0;
-    	 $MerchantRequestID=$KILI["Body"]["stkCallback"]["MerchantRequestID"];//confirmed
-         $CheckoutRequestID=$jsonMpesaResponse["Body"]["stkCallback"]["CheckoutRequestID"];
-         //$ResultCode=$jsonMpesaResponse["Body"]["stkCallback"]["ResultCode"];//confirmed
-         $ResultDesc=$jsonMpesaResponse["Body"]["stkCallback"]["ResultDesc"];
-         
-         //$CallbackMetadata=$jsonMpesaResponse["Body"]["stkCallback"]["CallbackMetadata"]["Item"];
-	    
-	    $Amount=$jsonMpesaResponse["Body"]["stkCallback"]["CallbackMetadata"]["Item"][0]["Value"];
-	    $ReceiptNumber=$jsonMpesaResponse["Body"]["stkCallback"]["CallbackMetadata"]["Item"]["stk_initiate"][1]["Value"];
-	    $TransactionDate=$jsonMpesaResponse["Body"]["stkCallback"]["CallbackMetadata"]["Item"][3]["Value"];
-	    $PhoneNumber=$jsonMpesaResponse["Body"]["stkCallback"]["CallbackMetadata"]["Item"][4]["Value"];
+  echo json_encode($response);
 
- if ($conn->connect_error)
- {
-   $request['status']= 1;
-  $request['message'] = "no connections to the server....";
-	
-
-  }
- else
- {
-    $request['status']= 0;
-    $request['message'] = "Connection was successfull...";
-
-   if($ResultCode===0){  
-	
-	   $query = "INSERT INTO mpesa_account (strOrderId) VALUES ('$AccountReference')";
-	   $query = mysqli_query($conn, $query);
-}
-else
-{
-	    //the transaction was not successfull...
-	$request['status_transaction']= 1;
-     	$request['message'] = "transaction failled";
-     }
-    
-}
-
-echo json_encode($request);
 ?>
