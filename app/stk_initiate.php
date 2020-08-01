52d9dc681fe1449419b6b98067082e9821ea7f11
LaxcoAds <65868300+Laxco@users.noreply.github.com>
2020-08-01 04:38:36 -0400

    Update stk_initiate.php

--- app/stk_initiate.php
+++ app/stk_initiate.php
@@ -1,51 +1,38 @@
 <?php
 if($_SERVER['REQUEST_METHOD']=='POST'){ 
-    
-  date_default_timezone_set('Africa/Nairobi');
   
   $strPhone = $_POST['strPhone'];
   $strAmount = $_POST['strAmount'];
-  $strOrderId = $_POST['strOrderId']; 
+  $strOrderId = $_POST['strOrderId'];  
+    
+  date_default_timezone_set('Africa/Nairobi');
 
   # access token
-  $consumerKey = 'tEU3DAg51BRejzXqUVGBn0AZvmbEs8Eu'; //Fill with your app Consumer Key
-  $consumerSecret = 'WrTBvQFpYsY7y4ov'; // Fill with your app Secret
+  $consumerKey = 'aLehRAMafEc4AMoKQRexcV3lK95qQbZ0'; //Fill with your app Consumer Key
+  $consumerSecret = 'naEbwu4q1iZ52JBE'; // Fill with your app Secret
 
   # define the variales
   # provide the following details, this part is found on your test credentials on the developer account
   $BusinessShortCode = '174379';
   $Passkey = 'bfb279f9aa9bdbcf158e97dd71a467cd2e0c893059b10f78e6b72ada1ed2c919';  
+
   
-  /*
-    This are your info, for
-    $PartyA should be the ACTUAL clients phone number or your phone number, format 2547********
-    $AccountRefference, it maybe invoice number, account number etc on production systems, but for test just put anything
-    TransactionDesc can be anything, probably a better description of or the transaction
-    $Amount this is the total invoiced amount, Any amount here will be 
-    actually deducted from a clients side/your test phone number once the PIN has been entered to authorize the transaction. 
-    for developer/test accounts, this money will be reversed automatically by midnight.
-  */
-  
-  //$PartyA = '254705146547'; // This is your phone number, 
- // $AccountReference = 'ASDDS';
+ 
   $TransactionDesc = 'Goods Payment';
-  //$Amount = '1';
  
   # Get the timestamp, format YYYYmmddhms -> 20181004151020
   $Timestamp = date('YmdHis');    
-  
   # Get the base64 encoded string -> $password. The passkey is the M-PESA Public Key
   $Password = base64_encode($BusinessShortCode.$Passkey.$Timestamp);
 
   # header for access token
   $headers = ['Content-Type:application/json; charset=utf8'];
-
-    # M-PESA endpoint urls
+    # M-PESA endpoint urls https://sandbox.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials
   $access_token_url = 'https://sandbox.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials';
   $initiate_url = 'https://sandbox.safaricom.co.ke/mpesa/stkpush/v1/processrequest';
 
   # callback url
-  $CallBackURL = 'https://pre-raphaelite-midw.000webhostapp.com/NyumbaDbScripts/M_pesa_APi/Tutorial%209/callback_url.php?strOrderId = '.$strOrderId; 
+  $CallBackURL = 'https://pre-raphaelite-midw.000webhostapp.com/NyumbaDbScripts/M_pesa_APi/Tutorial%209/callback_url.php?strOrderId='.$strOrderId;  
 
   $curl = curl_init($access_token_url);
   curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
@@ -55,16 +42,18 @@ if($_SERVER['REQUEST_METHOD']=='POST'){
   $result = curl_exec($curl);
   $status = curl_getinfo($curl, CURLINFO_HTTP_CODE);
   $result = json_decode($result);
-  $access_token = $result->access_token;  
+  $access_token = $result->access_token; 
+  echo $access_token;
   curl_close($curl);
 
   # header for stk push
-  $stkheader = ['Content-Type:application/json','Authorization:Bearer '.$access_token];
+  //$stkheader = ['Content-Type:application/json','Authorization:Bearer'.$access_token];
 
   # initiating the transaction
   $curl = curl_init();
   curl_setopt($curl, CURLOPT_URL, $initiate_url);
-  curl_setopt($curl, CURLOPT_HTTPHEADER, $stkheader); //setting custom header
+  //curl_setopt($curl, CURLOPT_HTTPHEADER,$stkheader);
+    curl_setopt($curl, CURLOPT_HTTPHEADER, array('Content-Type:application/json','Authorization:Bearer '.$access_token));//setting custom header
 
   $curl_post_data = array(
     //Fill in the request parameters with valid values
@@ -80,7 +69,6 @@ if($_SERVER['REQUEST_METHOD']=='POST'){
     'AccountReference' => $strOrderId,
     'TransactionDesc' => $TransactionDesc
   );
-  
 
   $data_string = json_encode($curl_post_data);
   curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
