8ac42bee3b845325e3b6b419fa2815b9329d4999
LaxcoAds <65868300+Laxco@users.noreply.github.com>
2020-08-07 13:08:37 +0300

    Add files via upload

---
+++ app/insertRivoNewUser.php
@@ -0,0 +1,33 @@
+<?php
+
+    require_once('dbConnect.php');
+    
+    $response = array(); 
+    
+    if(isset($_REQUEST['strUserId']) && isset($_REQUEST['strUsername']) 
+    && isset($_REQUEST['strPhone']) && isset($_REQUEST['strImageUrl']){
+        
+        	//Getting post data 
+		$strUserId = $_REQUEST['strUserId'];
+		$strUsername = $_REQUEST['strUsername'];
+		$strPhone = $_REQUEST['strPhone'];
+		$strImageUrl = $_REQUEST['strImageUrl'];
+
+        $adding = "INSERT INTO rivo_users (strUserId,strUsername,strPhone,strImageUrl) VALUES ('$strUserId','$strUsername','$strPhone','$strImageUrl')";
+       
+       $result = mysqli_query($conn,$adding);
+        
+    if ($result>0) {
+        // successfully inserted into database
+        $response["status"] = "0";
+        $response["message"] = "Successfuly sents.";
+    } else {
+        // failed to insert row
+        $response["status"] = "1";
+        $response["message"] = "Error: ".mysqli_error($con);
+    }
+    // echoing JSON response
+    echo json_encode($response);
+        
+    }
+?>
