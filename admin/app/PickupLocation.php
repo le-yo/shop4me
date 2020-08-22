c2c6ba7782584ac9613327ef369f13d6adf06293
Leonard Korir <lenyoskey@gmail.com>
2020-08-22 16:41:41 +0300

    moved StoredFIles to app

---
+++ admin/app/PickupLocation.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class PickupLocation extends Model
+{
+    protected $connection = 'mysql2';
+}
---
+++ admin/app/Shop.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Shop extends Model
+{
+    protected $connection = 'mysql2';
+    public $timestamps = false;
+}
