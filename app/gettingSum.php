69fe4c10b3c431b972e332d1dc59026486001efb
LaxcoAds <65868300+Laxco@users.noreply.github.com>
2020-09-09 16:49:04 +0300

    Update gettingSum.php

--- app/gettingSum.php
+++ app/gettingSum.php
@@ -2,16 +2,24 @@
 	
 	require_once('dbConnect.php');
 	
+	$strPostId = $_POST['strPostId'];
+	
 if (mysqli_connect_errno()) {
 		echo "Failed to connect to MySQL: " . mysqli_connect_error();
 		die();
 	}
 
-$sql = "SELECT SUM(strRate) FROM c_ratings WHERE";
-$result = $conn->query($sql);
-$rows = array();
-    while($temp = mysqli_fetch_assoc($result)) {
-        $rows[] = $temp;
-}
-    echo json_encode($rows);
+// $sql = "SELECT*, SUM(strRate) AS 'sum' FROM c_ratings WHERE strPostId = '$strPostId'";
+// $result = $conn->query($sql);
+// $rows = array();
+//     while($temp = mysqli_fetch_assoc($result)) {
+//         $rows[] = $temp;
+// }
+$query = mysqli_query($con, "SELECT *, SUM(strRate) AS 'sum' ROM c_ratings;");
+$row = mysqli_fetch_assoc($query);
+$sum = $row['sum'];
+	
+	
+    echo json_encode($sum);
+
 ?>
