2707036166953634764f3e6e6963b05ee38845b8
LaxcoAds <65868300+Laxco@users.noreply.github.com>
2020-09-09 14:40:34 +0300

    Update gettingCarracePeopleCount.php

--- app/gettingCarracePeopleCount.php
+++ app/gettingCarracePeopleCount.php
@@ -1,17 +1,21 @@
 <?php
+if($_SERVER['REQUEST_METHOD']=='POST'){
 	
 	require_once('dbConnect.php');
+	
+	$strPostId = $_POST['strPostId'];
 	
 if (mysqli_connect_errno()) {
 		echo "Failed to connect to MySQL: " . mysqli_connect_error();
 		die();
 	}
 
-$sql = "SELECT COUNT(*) FROM c_ratings WHERE strPostId = '260C9840O61078BK';";
+$sql = "SELECT COUNT(*) FROM c_ratings WHERE strPostId = '$strPostId';";
 $result = $conn->query($sql);
 $rows = array();
     while($temp = mysqli_fetch_assoc($result)) {
         $rows[] = $temp;
 }
     echo json_encode($rows);
+}
 ?>
