6a7752e6d6ef594ec4bcc36154d473ef38da7825
LaxcoAds <65868300+Laxco@users.noreply.github.com>
2020-09-09 15:30:34 +0300

    Add files via upload

---
+++ app/gettingCarraceRateSum.php
@@ -0,0 +1,21 @@
+<?php
+if($_SERVER['REQUEST_METHOD']=='POST'){
+	
+	require_once('dbConnect.php');
+	
+	$strPostId = $_POST['strPostId'];
+	
+if (mysqli_connect_errno()) {
+		echo "Failed to connect to MySQL: " . mysqli_connect_error();
+		die();
+	}
+
+$sql = "SELECT SUM(strRate) FROM c_ratings WHERE strPostId = '$strPostId';SUM(strRate)";
+$result = $conn->query($sql);
+$rows = array();
+    while($temp = mysqli_fetch_assoc($result)) {
+        $rows[] = $temp;
+}
+    echo json_encode($rows);
+}
+?>
