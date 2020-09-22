1b92403f805d125f08d43692beb67ed79d94cd55
LaxcoAds <65868300+Laxco@users.noreply.github.com>
2020-09-22 10:13:57 +0300

    Update dbConnectRe.php

--- app/dbConnectRe.php
+++ app/dbConnectRe.php
@@ -1,10 +1,9 @@
 
 <?php
-	
 	define('HOST','localhost');
-	define('USER','id14841061_root');
-	define('PASS','Mz~vE#2LgfGeLc!}');
-	define('DB','id14841061_projectzero');
+	define('USER','id12764483_laxcoshop');
+	define('PASS','zSVZAI[q1dOno05<');
+	define('DB','id12764483_shopforme');
 	
 	$conn = mysqli_connect(HOST,USER,PASS,DB) or die('Unable to Connect');
 ?>
