a34fda1a956272f117393ec42c95a921d5789c8a
LaxcoAds <65868300+Laxco@users.noreply.github.com>
2020-09-23 10:23:42 +0300

    Update insertingCarraceOrderItem.php

--- app/insertingCarraceOrderItem.php
+++ app/insertingCarraceOrderItem.php
@@ -35,7 +35,7 @@
     } else {
         // failed to insert row
         $response["status"] = "1";
-        $response["message"] = "Error: ".mysqli_error($con);
+        $response["message"] = "Error: ".mysqli_error($conn);
     }
     // echoing JSON response
     echo json_encode($response);
