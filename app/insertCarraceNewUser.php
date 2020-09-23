5003c19c5d2313975b68644ee128bff8da9e2beb
LaxcoAds <65868300+Laxco@users.noreply.github.com>
2020-09-23 11:57:53 +0300

    Update insertCarraceNewUser.php

--- app/insertCarraceNewUser.php
+++ app/insertCarraceNewUser.php
@@ -33,7 +33,7 @@
     } else {
         // failed to insert row
         $response["status"] = "1";
-        $response["message"] = "Error: ".mysql_error($result);
+        $response["message"] = "Error: ".mysqli_error($result);
     }
     // echoing JSON response
     echo json_encode($response);
